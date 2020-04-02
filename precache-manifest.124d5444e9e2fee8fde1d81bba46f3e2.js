c22cba5e85ff0c4985939c76f1904991650b6c20
MHMighani <mh.mgh2012@gmail.com>
2020-04-02 21:23:39 +0430

    Updates

---
+++ precache-manifest.124d5444e9e2fee8fde1d81bba46f3e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "392024a418614a62bbd8a079c3de0da0",
+    "url": "/My-resume/index.html"
+  },
+  {
+    "revision": "4a2a0d880cbb8e2bdd1c",
+    "url": "/My-resume/static/css/main.17765250.chunk.css"
+  },
+  {
+    "revision": "51f0afcf7e6b4403d889",
+    "url": "/My-resume/static/js/2.3e3b152e.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/My-resume/static/js/2.3e3b152e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a2a0d880cbb8e2bdd1c",
+    "url": "/My-resume/static/js/main.7dbeff8c.chunk.js"
+  },
+  {
+    "revision": "c04eacfb3e4de3e672f9",
+    "url": "/My-resume/static/js/runtime-main.03f55d7c.js"
+  }
+]);
